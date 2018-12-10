11db3888e1a44e4f6ac7e4ce9a773a68e9f7f06e
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-10 16:07:36 -0400

    Finished the challenges while only robbing some styling from Jonas final file, my input was horrible, game now works with 2 dices and resets whole score if you get 2 six in a row with any dice

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,34 +9,68 @@ GAME RULES:
 
 */
 
-var puntajes, puntajeRonda, jugadorAct, dado;
+var puntajes, puntajeRonda, jugadorAct, playing, lastRoll0, lastRoll1, winScore;
 
 initGame();
 
-document.querySelector('.btn-roll').addEventListener('click', function(){
+function roll(num) {
+    if (num === 1 && document.querySelector('.dice0').style.display == 'none'){
+        return;
+    }
     var dado = Math.floor(Math.random() * 6) + 1;
-    var diceDOM = document.querySelector('.dice');
+    var diceDOM = document.querySelector('.dice' + num);
+    var lastRoll;
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dado + '.png';
+    if (num === 0) {
+        lastRoll = lastRoll0;
+        lastRoll0 = dado;
+    } else {
+        lastRoll = lastRoll1;
+        lastRoll1 = dado;
+    }
+    if (lastRoll === 6 && dado === 6) {
+        puntajes[jugadorAct] = 0;
+        document.querySelector('#score-'+jugadorAct).textContent = '0';
+        nextPlayer();
+        return;
+    }
     if (dado !== 1) {
         puntajeRonda += dado;
         document.querySelector('#current-'+jugadorAct).textContent = puntajeRonda;
     } else {
         nextPlayer();
     }
+}
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    if (playing) {
+        roll(0);
+        roll(1);
+    }
+});
+
+document.querySelector('.input').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+        winScore = e.target.value
+        document.querySelector('.input').value = '';
+    }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-    if (puntajeRonda) {
+    if (puntajeRonda && playing) {
         puntajes[jugadorAct] += puntajeRonda;
         document.querySelector('#score-'+jugadorAct).textContent = puntajes[jugadorAct];
-        if (puntajes[jugadorAct] >= 20) {
+        if (puntajes[jugadorAct] >= winScore) {
             document.querySelector('#name-' + jugadorAct).textContent = 'WINNER!';
-            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.dice0').style.display = 'none';
+            document.querySelector('.dice1').style.display = 'none';
             document.querySelector('.player-' + jugadorAct + '-panel').classList.add('winner');
             document.querySelector('.player-' + jugadorAct + '-panel').classList.remove('active');
             document.querySelector('.btn-roll').style.display = 'none';
             document.querySelector('.btn-hold').style.display = 'none';
+            playing = false;
         } else {
             nextPlayer();
         }
@@ -49,19 +83,16 @@ function initGame() {
     puntajes = [0,0];
     puntajeRonda = 0;
     jugadorAct = 0;
-
-    // dado = Math.floor(Math.random() * 6) + 1;
-    // // console.log(dado);
-
-    // document.querySelector('#current-'+jugadorAct).textContent = dado;
-    // //document.querySelector('#current-'+jugadorAct).innerHTML = '<em>' +dado+'</em>';
-
-    // puntajes[0] = document.querySelector('#score-0').textContent;
+    playing = true;
+    lastRoll0 = 0;
+    lastRoll1 = 0;
+    winScore = 20;
 
     document.querySelector('.player-0-panel').classList.add('active');
     document.querySelector('.player-1-panel').classList.remove('active');
 
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice0').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
     document.querySelector('.btn-roll').style.display = 'block';
     document.querySelector('.btn-hold').style.display = 'block';
 
@@ -81,9 +112,12 @@ function initGame() {
 function nextPlayer() {
     jugadorAct === 0 ? jugadorAct = 1 : jugadorAct = 0;
     puntajeRonda = 0;
+    lastRoll1 = 0;
+    lastRoll0 = 0;
     document.querySelector('#current-0').textContent = '0';
     document.querySelector('#current-1').textContent = '0';
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice0').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
 }
