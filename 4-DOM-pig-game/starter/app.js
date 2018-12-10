81666fccee669477f4bcdd9c63e02628775c0337
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-10 13:37:43 -0400

    made init function for new games and at play start

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,25 +11,7 @@ GAME RULES:
 
 var puntajes, puntajeRonda, jugadorAct, dado;
 
-puntajes = [0,0];
-puntajeRonda = 0;
-jugadorAct = 0;
-
-// dado = Math.floor(Math.random() * 6) + 1;
-// // console.log(dado);
-
-// document.querySelector('#current-'+jugadorAct).textContent = dado;
-// //document.querySelector('#current-'+jugadorAct).innerHTML = '<em>' +dado+'</em>';
-
-puntajes[0] = document.querySelector('#score-0').textContent;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
+initGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     var dado = Math.floor(Math.random() * 6) + 1;
@@ -48,16 +30,53 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     if (puntajeRonda) {
         puntajes[jugadorAct] += puntajeRonda;
         document.querySelector('#score-'+jugadorAct).textContent = puntajes[jugadorAct];
-        if (puntajes[jugadorAct] >= 100) {
+        if (puntajes[jugadorAct] >= 20) {
             document.querySelector('#name-' + jugadorAct).textContent = 'WINNER!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + jugadorAct + '-panel').classList.add('winner');
             document.querySelector('.player-' + jugadorAct + '-panel').classList.remove('active');
+            document.querySelector('.btn-roll').style.display = 'none';
+            document.querySelector('.btn-hold').style.display = 'none';
         } else {
             nextPlayer();
         }
     }
 });
+
+document.querySelector('.btn-new').addEventListener('click', initGame);
+
+function initGame() {
+    puntajes = [0,0];
+    puntajeRonda = 0;
+    jugadorAct = 0;
+
+    // dado = Math.floor(Math.random() * 6) + 1;
+    // // console.log(dado);
+
+    // document.querySelector('#current-'+jugadorAct).textContent = dado;
+    // //document.querySelector('#current-'+jugadorAct).innerHTML = '<em>' +dado+'</em>';
+
+    // puntajes[0] = document.querySelector('#score-0').textContent;
+
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.btn-roll').style.display = 'block';
+    document.querySelector('.btn-hold').style.display = 'block';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+}
 
 function nextPlayer() {
     jugadorAct === 0 ? jugadorAct = 1 : jugadorAct = 0;
