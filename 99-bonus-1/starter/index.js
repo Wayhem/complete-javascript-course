b8670f8b3453668f683180935425877532835f86
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-13 09:50:06 -0400

    started templates, (without express routing and templating would be a pain!)

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -3,16 +3,28 @@ const http = require('http');
 const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
-const data = JSON.parse(json);
+const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
     if (pathName === '/products' || pathName === '/'){
         res.writeHead(200, { 'Content-type': 'text/html' });
         res.end('products page!');
-    } else if (pathName === '/laptop'){
+    } else if (pathName === '/laptop' && id < laptopData.length && id >= 0){
         res.writeHead(200, { 'Content-type': 'text/html' });
-        res.end('laptop page!');
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+            output = output.replace(/{%IMAGE%}/g, laptop.image);
+            output = output.replace(/{%PRICE%}/g, laptop.price);
+            output = output.replace(/{%SCREEN%}/g, laptop.screen);
+            output = output.replace(/{%CPU%}/g, laptop.cpu);
+            output = output.replace(/{%STORAGE%}/g, laptop.storage);
+            output = output.replace(/{%RAM%}/g, laptop.ram);
+            output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+            res.end(output);
+        });
     } else {
         res.writeHead(404, { 'Content-type': 'text/html' });
         res.end('not found!');
