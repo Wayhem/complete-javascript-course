5b7d21e4b6c230ddf4bc04c118d09242a08dd933
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-13 01:20:32 -0400

    enable routing on the small node app

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -1,11 +1,22 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const data = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-    console.log('Server is accesible');
+    const pathName = url.parse(req.url, true).pathname;
+    if (pathName === '/products' || pathName === '/'){
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end('products page!');
+    } else if (pathName === '/laptop'){
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end('laptop page!');
+    } else {
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.end('not found!');
+    }
 });
 
 server.listen(1337, '127.0.0.1', () => {
