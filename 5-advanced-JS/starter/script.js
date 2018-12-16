cc85deed81c6466846f1de08a9bddc08f1b046e4
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-16 13:19:50 -0400

    finished JS challenge, had problems figuring out how to use effectively closures, rest was really easy, reviewing has been smooth

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,16 +1,60 @@
-function Question(question, arrayAns, answer) {
-  this.question = question;
-  this.arrayAns = arrayAns;
-  this.answer = answer;
-}
+(function(){
+  function Question(question, arrayAns, answer) {
+    this.question = question;
+    this.arrayAns = arrayAns;
+    this.answer = answer;
+  }
 
-let arrayQuestions = [];
-let question1 = new Question('Blondes Are?', ["Smart", "Really smart", "Dumb"], 2);
-let question2 = new Question('Beagle is a?', ["Small breed", "Medium breed", "Large breed"], 0);
-let question3 = new Question('Hitler was from?', ["France", "Germany", "Austria"], 2);
+  function closure(){
+    let sc = 0;
+    return function(status) {
+      if (status) {
+        sc++;
+      } else {
+        return sc;
+      }
+    }
+  }
 
-arrayQuestions.push(question1);
-arrayQuestions.push(question2);
-arrayQuestions.push(question3);
+  Question.prototype.ask = function () {
+    console.log(this.question);
+    for (let i = 0; i<this.arrayAns.length; i++) {
+      console.log(i+': '+ this.arrayAns[i]);
+    }
+    console.log('Score: ' + score(false));
+    this.respond();
+  };
 
-console.log(arrayQuestions);
+  Question.prototype.respond = function () {
+    let answer = prompt('Which is the correct answer?');
+    if (answer == this.answer) {
+      alert('CORRECT!');
+      score(true);
+      this.redo();
+    } else if(answer == 'exit'){
+      return;
+    }
+    else {
+      alert('WRONG!');
+      this.redo();
+    }
+  };
+
+  Question.prototype.redo = function() {
+    let random = Math.floor(Math.random() * arrayQuestions.length);
+    console.clear();
+    arrayQuestions[random].ask();
+  };
+
+  let arrayQuestions = [];
+  let question1 = new Question('Blondes Are?', ["Smart", "Really smart", "Dumb"], 2);
+  let question2 = new Question('Beagle is a?', ["Small breed", "Medium breed", "Large breed"], 0);
+  let question3 = new Question('Hitler was from?', ["France", "Germany", "Austria"], 2);
+  let score = closure();
+
+  arrayQuestions.push(question1);
+  arrayQuestions.push(question2);
+  arrayQuestions.push(question3);
+  let random = Math.floor(Math.random() * arrayQuestions.length);
+  arrayQuestions[random].ask();
+})();
