7bfdde1c340bdf3c4440e34cb6582da434f262c0
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-18 17:18:33 -0400

    Finished data structuration even with no persistance whatsoever for this small app, now input arrives to data controller, next is update income/expense list on ui, calculate and then display budget.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,6 +1,43 @@
 var budgetController = (function(){
     
+    var Income = function(id, description, amount){
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+    }
+    
+    var Expense = function(id, description, amount){
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+    }
 
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem;
+
+            ID = 0;
+
+            if (type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            console.log(newItem);
+        }
+    }
 
 })();
 
@@ -27,23 +64,33 @@ var UIController = (function(){
 })();
 
 const controller = (function(budgetCtrl, UICtrl){
+    var setupEventListeners = function() {
+        const DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(e){
+            if (e.keyCode === 13 || e.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
     var ctrlAddItem = function () {
         //get input
         var input = UICtrl.getInput();
-        console.log(input);
         //add input to budgetcontroller (data)
+        budgetController.addItem(input.type, input.description, input.value);
         //add item to UIcontroller (ui)
         //calculate budget
         //display budget on ui
-    }
-    const DOM = UICtrl.getDOMstrings;
+    };
 
-    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(e){
-        if (e.keyCode === 13 || e.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function(){
+            console.log('app started');
+            setupEventListeners();
         }
-    });
+    };
 
 })(budgetController, UIController);
+
+controller.init();
