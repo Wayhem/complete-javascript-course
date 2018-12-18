c0a619a71a68653622aa4dd0e261bbd9b78a1d51
Jose Villarroel <oscarclarke9900@gmail.com>
2018-12-18 14:34:10 -0400

    modular architecture set, View controller, Data controller, and middle controller (some kind of local MVC), right now UI controller gets input and middle controller is able to log them

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,12 +1,40 @@
-const budgetController = (function(){
-    var x = 23;
-    var add = function(a) {
-        return x + a;
-    }
+var budgetController = (function(){
+    
+
+
+})();
+
+var UIController = (function(){
 
     return {
-        publicTest: function(b) {
-            console.log(add(b));
+        getInput: function(){
+            return {
+                type: document.querySelector('.add__type').value, //inc or exp
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            };
         }
-    }
+    };
+
 })();
+
+const controller = (function(budgetCtrl, UICtrl){
+    var ctrlAddItem = function () {
+        //get input
+        var input = UICtrl.getInput();
+        console.log(input);
+        //add input to budgetcontroller (data)
+        //add item to UIcontroller (ui)
+        //calculate budget
+        //display budget on ui
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(e){
+        if (e.keyCode === 13 || e.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
+})(budgetController, UIController);
